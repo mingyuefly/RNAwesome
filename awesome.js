f87270a6c7f70676928f3f57d3bb8dad2ae32cc1
guomingyue <mingyuefly@qq.com>
2019-07-17 18:46:28 +0800

    Align Items

--- awesome.js
+++ awesome.js
@@ -9,13 +9,13 @@
 import React, {Component} from 'react';
 import {AppRegistry, View} from 'react-native';
 
-export default class FlexDirectionBasics extends Component {
+export default class AlignItemsBasics extends Component {
   render() {
     return (
-      <View style = {{flex:1, flexDirection:'column', justifyContent:'space-between'}}>
+      <View style = {{flex:1, flexDirection:'column', justifyContent:'center', alignItems:'stretch'}}>
         <View style = {{width:50, height:50, backgroundColor:'powderblue'}} />
-        <View style = {{width:50, height:50, backgroundColor:'skyblue'}} />
-        <View style = {{width:50, height:50, backgroundColor:'steelblue'}} />
+        <View style = {{height:50, backgroundColor:'skyblue'}} />
+        <View style = {{height:100, backgroundColor:'steelblue'}} />
       </View>
     );
   }
