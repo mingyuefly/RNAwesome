52e9c9210adeed0ea72a904b893ef5c06bdaca36
guomingyue <mingyuefly@qq.com>
2019-07-17 16:48:07 +0800

    state

--- awesome.js
+++ awesome.js
@@ -9,23 +9,35 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 
-class Greeting extends Component {
-  render () {
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {isShowingText:true};
+    setInterval(() => {
+      this.setState(previousState => {
+        return {isShowingText:!previousState.isShowingText}
+      });
+    }, 1000);
+  }
+  
+  render() {
+    if (!this.state.isShowingText) {
+      return null;
+    }
     return (
-    <View style = {{alignItems: 'center', margin:50}}>
-      <Text>Hello {this.props.name}</Text>
-    </View>
+      <Text>{this.props.text}</Text>
     );
-  };
+  }
 }
 
-export default class LotsOfGreetings extends Component {
+export default class BlinkApp extends Component {
   render() {
     return (
-      <View style = {{alignItems:"center"}}>
-        <Greeting name = 'Rexxar' />
-        <Greeting name = 'Jaina' />
-        <Greeting name = 'Valeera' />
+      <View>
+        <Blink text = 'I love to blink' />
+        <Blink text = 'Yes blinking is so great' />
+        <Blink text = 'Why did they ever take this out of HTML' />
+        <Blink text = 'Look at me look at me look at me' />
       </View>
     );
   }
