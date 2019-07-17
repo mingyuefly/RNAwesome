b72c089b0476b9306ce3cf5caa4eb41a2384ee45
guomingyue <mingyuefly@qq.com>
2019-07-17 17:37:50 +0800

    styles

--- awesome.js
+++ awesome.js
@@ -7,38 +7,29 @@
  */
 
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {AppRegistry, StyleSheet, Text, View} from 'react-native';
 
-class Blink extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {isShowingText:true};
-    setInterval(() => {
-      this.setState(previousState => {
-        return {isShowingText:!previousState.isShowingText}
-      });
-    }, 1000);
-  }
-  
-  render() {
-    if (!this.state.isShowingText) {
-      return null;
-    }
-    return (
-      <Text>{this.props.text}</Text>
-    );
-  }
-}
-
-export default class BlinkApp extends Component {
+export default class LotsOfStyles extends Component {
   render() {
     return (
       <View>
-        <Blink text = 'I love to blink' />
-        <Blink text = 'Yes blinking is so great' />
-        <Blink text = 'Why did they ever take this out of HTML' />
-        <Blink text = 'Look at me look at me look at me' />
+        <Text style = {styles.red}>just red</Text>
+        <Text style = {styles.bigblue}>just bigblue</Text>
+        <Text style = {[styles.bigblue, styles.red]}>bigblue, then red</Text>
+        <Text style = {[styles.red, styles.bigblue]}>red, then bigblue</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  bigblue:{
+    color:'blue',
+    fontWeight:'bold',
+    fontSize:30,
+  },
+  red:{
+    color:'red',
+  },
+});
+
