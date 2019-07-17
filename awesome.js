2333b21c2263f1f6359f705145fe3cac80ee4b6d
guomingyue <mingyuefly@qq.com>
2019-07-17 18:39:43 +0800

    justify content

--- awesome.js
+++ awesome.js
@@ -12,7 +12,7 @@ import {AppRegistry, View} from 'react-native';
 export default class FlexDirectionBasics extends Component {
   render() {
     return (
-      <View style = {{flex:1, flexDirection:'row'}}>
+      <View style = {{flex:1, flexDirection:'column', justifyContent:'space-between'}}>
         <View style = {{width:50, height:50, backgroundColor:'powderblue'}} />
         <View style = {{width:50, height:50, backgroundColor:'skyblue'}} />
         <View style = {{width:50, height:50, backgroundColor:'steelblue'}} />
