c8af3a56d6bbb2000b2b0d671a18a5329c1eb865
guomingyue <mingyuefly@qq.com>
2019-07-17 15:19:47 +0800

    自定义组件和propos

--- awesome.js
+++ awesome.js
@@ -7,13 +7,26 @@
  */
 
 import React, {Component} from 'react';
-import {Image} from 'react-native';
+import {Text, View} from 'react-native';
 
-export default class Bananas extends Component {
-  render() {
-    let pic = {uri : 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'};
+class Greeting extends Component {
+  render () {
     return (
-      <Image source = {pic} style = {{width:193, height:110}} />
+    <View style = {{alignItems: 'center', margin:50}}>
+      <Text>Hello {this.props.name}</Text>
+    </View>
+    );
+  };
+}
+
+export default class LotsOfGreetings extends Component {
+  render() {
+    return (
+      <View style = {{alignItems:"center"}}>
+        <Greeting name = 'Rexxar' />
+        <Greeting name = 'Jaina' />
+        <Greeting name = 'Valeera' />
+      </View>
     );
   }
 }
