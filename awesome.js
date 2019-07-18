a0bab4c7eaf8fd9e944613b74e6a1aa3c00aeb50
guomingyue <mingyuefly@qq.com>
2019-07-18 10:49:00 +0800

    TextInput

--- awesome.js
+++ awesome.js
@@ -7,19 +7,25 @@
  */
 
 import React, {Component} from 'react';
-import {AppRegistry, View} from 'react-native';
+import {AppRegistry, Text, TextInput, View} from 'react-native';
+import { placeholder } from '@babel/types';
 
-export default class AlignItemsBasics extends Component {
+export default class UselessTextInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text:''};
+  }
+
   render() {
-    return (
-      <View style = {{flex:1, flexDirection:'column', justifyContent:'center', alignItems:'stretch'}}>
-        <View style = {{width:50, height:50, backgroundColor:'powderblue'}} />
-        <View style = {{height:50, backgroundColor:'skyblue'}} />
-        <View style = {{height:100, backgroundColor:'steelblue'}} />
+    return(
+      <View style = {{flex:1, flexDirection:'column', justifyContent:'center', padding:10}}>
+        <TextInput style = {{height:40}} placeholder = "Type here to translate" onChangeText = {(text)=>this.setState({text})} />
+        <Text style = {{padding:10, fontSize:42}}>
+          {this.state.text.split(' ').map((word)=>word && '🍕').join(' ')}
+        </Text>
       </View>
-    );
+    )
   }
 }
-
 
 
