9d4a0b5ee6e92b572edb49362d4f2190fcf4f825
guomingyue <mingyuefly@qq.com>
2019-07-18 15:52:00 +0800

    Button

--- awesome.js
+++ awesome.js
@@ -7,25 +7,45 @@
  */
 
 import React, {Component} from 'react';
-import {AppRegistry, Text, TextInput, View} from 'react-native';
-import { placeholder } from '@babel/types';
+import {Alert, AppRegistry, Button, StyleSheet, View} from 'react-native';
 
-export default class UselessTextInput extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {text:''};
+export default class ButtonBasics extends Component {
+  _onPressButton() {
+    Alert.alert('You tapped the button');
   }
 
   render() {
-    return(
-      <View style = {{flex:1, flexDirection:'column', justifyContent:'center', padding:10}}>
-        <TextInput style = {{height:40}} placeholder = "Type here to translate" onChangeText = {(text)=>this.setState({text})} />
-        <Text style = {{padding:10, fontSize:42}}>
-          {this.state.text.split(' ').map((word)=>word && '🍕').join(' ')}
-        </Text>
+    return (
+      <View style = {styles.container}>
+        <View style = {styles.buttonContainer}>
+          <Button onPress = {this._onPressButton} title = "Press me" />
+        </View>
+        <View style = {styles.buttonContainer}>
+          <Button onPress = {this._onPressButton} title = "Press me" color = "#841584" />
+        </View>
+        <View style = {styles.alternativeLayoutButtonContainer}>
+          <Button onPress = {this._onPressButton} title = "This looks great" />
+          <Button onPress = {this._onPressButton} title = "OK!" color = "#841584" />
+        </View>
       </View>
-    )
+    );
   }
 }
 
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'center',
+  },
+  buttonContainer:{
+    margin:20,
+  },
+  alternativeLayoutButtonContainer:{
+    margin:20,
+    flexDirection:'row',
+    justifyContent:'space-between',
+  },
+})
+
+//AppRegistry.registerComponent
 
