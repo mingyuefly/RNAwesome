8396925546165106ff982a39f8778ccc98a512c0
guomingyue <mingyuefly@qq.com>
2019-07-18 19:21:19 +0800

    SectionList

--- awesome.js
+++ awesome.js
@@ -7,14 +7,17 @@
  */
 
 import React, {Component} from 'react';
-import {FlatList, StyleSheet, Text, View} from 'react-native';
+import {SectionList, StyleSheet, Text, View} from 'react-native';
 
-export default class FlatListBasics extends Component {
+export default class SectionListBasics extends Component {
   render() {
     return (
       <View style = {styles.container}>
-        <FlatList data = {[{key:'Devin'}, {key:'Jackson'}, {key:'James'}, {key:'Joel'}, {key:'John'}, {key:'Jillian'}, {key:'Jimmy'}, {key:'Julie'}]}
-        renderItem = {({item})=><Text style = {styles.item}>{item.key}</Text>} />
+        <SectionList sections = {[{title: 'D', data: ['Devin']},
+            {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},]}
+            renderItem = {({item})=><Text style = {styles.item}>{item}</Text>}
+            renderSectionHeader = {(section)=><Text style = {styles.sectionHeader}>{section.title}</Text>}
+            keyExtractor = {(item, index)=>index} />
       </View>
     );
   }
@@ -23,7 +26,16 @@ export default class FlatListBasics extends Component {
 const styles = StyleSheet.create({
   container:{
     flex:1,
-    paddingTop:42,
+    paddingTop:22,
+  },
+  sectionHeader:{
+    paddingTop:2,
+    paddingLeft:10,
+    paddingRight:10,
+    paddingBottom:2,
+    fontSize:14,
+    fontWeight:'bold',
+    backgroundColor:'rgba(247, 247, 247, 1.0)',
   },
   item:{
     padding:10,
