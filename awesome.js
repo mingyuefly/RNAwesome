0073a01101b4847fb80cfdbd6dcd70251f2f67f5
guomingyue <mingyuefly@qq.com>
2019-07-19 18:11:40 +0800

    movies

--- awesome.js
+++ awesome.js
@@ -7,39 +7,96 @@
  */
 
 import React from 'react';
-import {FlatList, ActivityIndicator, Text, View} from 'react-native';
+import {Image, FlatList, StyleSheet, Text, View} from 'react-native';
 
-export default class FetchExample extends React.Component {
+var REQUEST_URL = "https://raw.githubusercontent.com/facebook/react-native/0.51-stable/docs/MoviesExample.json";
+
+export default class SampleAppMovies extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {isLoading:true};
+    this.state = {
+      data:[],
+      loaded:false,
+    };
+    this.fetchData = this.fetchData.bind(this);
   }
 
   componentDidMount() {
-    return fetch('https://facebook.github.io/react-native/movies.json').then((response)=>response.json()).then((responseJson)=>{
-      this.setState({
-        isLoading:false,
-        dataSource:responseJson.movies,
-      }, function(){
+    this.fetchData();
+  }
 
-      })
-    }).catch((error)=>{
-      console.error(error);
+  fetchData() {
+    fetch(REQUEST_URL).then(response=>response.json()).then(responseData=>{
+      this.setState({
+        data:this.state.data.concat(responseData.movies),
+        loaded:true
+      });
     });
   }
 
   render() {
-    if(this.state.isLoading) {
-      return(
-        <View style = {{flex:1, padding:20}}>
-          <ActivityIndicator />
-        </View>
-      );
+    if (!this.state.loaded) {
+      return this.renderLoadView();
     }
-    return(
-      <View style = {{flex:1, padding:20}}>
-        <FlatList data = {this.state.dataSource} renderItem = {({item})=><Text>{item.title},{item.releaseYear}</Text>} />
+    return (
+      <FlatList>
+        data = {this.state.data}
+        renderItem = {this.renderMovie}
+        style = {styles.list}
+        keyExtractor = {item=>item.id}
+      </FlatList>
+    );
+  }
+
+  renderLoadView() {
+    return (
+      <View style = {styles.container}>
+        <Text>Loading movies......</Text>
+      </View>
+    );
+  }
+
+  renderMovie({item}) {
+    return (
+      <View style = {styles.container}>
+        <Image source = {{uri:item.posters.thumbnail}} style = {styles.thumbnail} />
+        <View style = {styles.rightContainer}>
+          <Text style = {styles.title}>{item.title}</Text>
+          <Text style = {styles.year}>{item.year}</Text>
+        </View>
       </View>
     );
   }
 }
+
+var styles = StyleSheet.create({
+  container:{
+    flex:1,
+    flexDirection:"row",
+    justifyContent:"center",
+    alignItems:"center",
+    backgroundColor:"#F5FCFF",
+  },
+  rightContainer:{
+    flex:1,
+  },
+  title:{
+    fontSize:20,
+    marginBottom:8,
+    textAlign:"center",
+  },
+  year:{
+    textAlign:"center"
+  },
+  thumbnail:{
+    width:53,
+    height:81,
+  },
+  list:{
+    paddingTop:20,
+    backgroundColor:"#F5FCFF",
+  },
+})
+
+
+
